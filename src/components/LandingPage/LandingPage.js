f85fb27c55cea6be4b0acee6baafa30c5528eee7
Mujeeb-web <mujeebisnot@hotmail.com>
2023-02-19 01:23:45 -0800

    Mujeeb

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -9,7 +9,13 @@ function LandingPage() {
   return (
     
     <div>
-      <div className='fade-in-out'>Story line starts with fade in fade out</div>
+      <div className='fade-in-and-fade-out'>Game description:
+      This is a modified word scrambling game. The objective is to rearrange the jumbled words before the time runs out.
+      </div>
+      <div className='fade-in'>
+      Two friends, rabbit and snake, hike up a mountain.
+      When they reach the top, the snake disappears. Rabbit feels scared without the snake.
+      A ghost appears and shouts at rabbit. Now, you must translate these words to continue your journey to safety.</div>
       <div className="container"> </div>
       {/* If storyline has finished telling, then show the instruction paragraph and button */}
       <button className='Button' onClick={() => navigate('/game')}>Start</button>
